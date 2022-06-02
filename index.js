f85b5b46482df95cd8e5068da6b4dea29d5e6c2b
kalunge <muthomititus+github@gmail.com>
2022-06-02 20:25:49 +0300

    Add solution

---
+++ index.js
@@ -0,0 +1,14 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const increment = (n) => n + 1;
+const decrement = (n) => n - 1;
+
+const makeInt = (n) => {
+  return parseFloat(n);
+};
+
+const preserveDecimal = (n) => {
+  return parseFloat(n);
+};
